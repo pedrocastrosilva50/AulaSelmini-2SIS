f50524f6b1eb97026084807d32abbe9d3da0edae
labsfiap <pedrocastrosilva50@gmail.com>
2026-02-24 20:21:51 -0300

    Aula dia 24/02

---
+++ src/Aula4ExMedico/Main.java
@@ -0,0 +1,23 @@
+package Aula4ExMedico;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Paciente paciente = new Paciente();
+        int freq = paciente.frequenciaMaxima();
+        double[]freqA = paciente.frequenciaAlvo();
+
+        System.out.print("Digite seu nome: ");
+        paciente.nome = sc.next();
+        System.out.print("Digite sua idade: ");
+        paciente.idade = sc.nextInt();
+
+        System.out.println("");
+
+        System.out.println("Sua frequência cardiaca máxima é "+freq);
+        System.out.print("Sua frequência cardiaca alvo está entre " +freqA[0]+ " e "+freqA[1]);
+
+    }
+}
---
+++ src/Aula4ExMedico/Paciente.java
@@ -0,0 +1,18 @@
+package Aula4ExMedico;
+
+public class Paciente {
+    String nome;
+    int idade;
+
+    public int frequenciaMaxima(){
+        return 220 - idade;
+    }
+
+    public double [] frequenciaAlvo() {
+        double[]alvo = new double[2];
+        int fm = frequenciaMaxima();
+        alvo[0] = frequenciaMaxima() * 0.5;
+        alvo[1] = frequenciaMaxima() * 0.85;
+        return alvo;
+    }
+}
